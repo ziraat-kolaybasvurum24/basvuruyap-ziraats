7a4f073b99aec7f91af3e012ab441583442bca0c
Aligollez <59908197+aligollez@users.noreply.github.com>
2023-11-08 21:26:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
-const express = require('express');
+require('express');
 require('dotenv').config();
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const {readdirSync} = require("fs");
-
-app.use(cors()); // CORS desteğini ekleyin
+const {readdirSync} = require("fs")
 app.use(express.json());
+
 app.use(express.static('public'));
 
-readdirSync("./routes").map((file) => app.use("/", require("./routes/" + file));
 
-app.get('styles.css', (req, res) => {
+readdirSync("./routes").map((file)=>app.use("/",require("./routes/"+file)))
+ app.get('styles.css', (req, res) => {
   res.sendFile(__dirname + '/public/styles.css');
 });
 
-app.get('/', (req, res) => {
+
+ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-
 app.get('/onay', (req, res) => {
   res.sendFile(__dirname + '/public/onay.html');
 });
-
-app.get('onay.png', (req, res) => {
+ app.get('onay.png', (req, res) => {
   res.sendFile(__dirname + '/public/onay.png');
 });
-
-app.get('bg.png', (req, res) => {
+ app.get('bg.png', (req, res) => {
   res.sendFile(__dirname + '/public/bg.png');
 });
-
-app.get('head.png', (req, res) => {
+ app.get('head.png', (req, res) => {
   res.sendFile(__dirname + '/public/head.png');
 });
-
-app.listen(port, () => {
+ app.listen(port, () => {
   console.log(`Web sunucusu ${port} adresinde çalışıyor.`);
 });
