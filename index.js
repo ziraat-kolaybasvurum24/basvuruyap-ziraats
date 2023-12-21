c9f7bd1a94d2eb2591312940b5ef8192eaeee910
Aligollez <59908197+aligollez@users.noreply.github.com>
2023-12-21 09:49:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
- const express = require('express');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
-const {readdirSync} = require("fs")
+const {readdirSync} = require("fs");
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static('public'));
